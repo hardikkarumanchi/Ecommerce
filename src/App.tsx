b9e403786cfb063260e8c39b039f60d7d33a248a
Hardik Karumanchi <hardik.karumanchi@hardikkarumanchi.local>
2026-02-03 11:56:29 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import './App.css'
+import Login from './pages/Login.tsx';
+import Signup from './pages/Signup.tsx'; 
+import Cart from './pages/Cart.tsx'; 
+import Home from './pages/Home.tsx'; 
+import Orders from './pages/Orders.tsx'; 
+import Admin from './pages/Admin.tsx';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
+
+function App() {
+
+  return (
+     <Router>
+      <Routes>
+      <Route path="/home" element={<Home />} />
+      <Route path = "/login" element = {<Login />} />
+      <Route path = "/signup" element = {<Signup />} />
+      <Route path = "/orders" element = {<Orders />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/admin" element={<Admin />} />
+      </Routes>
+
+     </Router>
+  );
+}
+
+export default App
