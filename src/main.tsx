04f0081a65202cf16e2439678fd21a31b533bf05
hardikkarumanchi <hardik.karumanchi@anywhere.co>
2026-02-16 14:31:36 +0530

    Revert "Redux branch" (#2)

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import { Provider } from 'react-redux';
-import { store } from './app/store';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* Wrap your App in the Provider and pass the store */}
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </StrictMode>
+    <App />
+  </StrictMode>,
 )
